a244297656fe1fa47199635be195853eeebdf137
ngu04 <nilesh.gupta@bskyb.com>
2015-09-08 16:24:54 +0100

    removed require js

--- es5-sham.js
+++ es5-sham.js
@@ -9,25 +9,7 @@
 // Add semicolon to prevent IIFE from being passed as argument to concatenated code.
 ;
 
-// UMD (Universal Module Definition)
-// see https://github.com/umdjs/umd/blob/master/returnExports.js
-(function (root, factory) {
-    'use strict';
-
-    /* global define, exports, module */
-    if (typeof define === 'function' && define.amd) {
-        // AMD. Register as an anonymous module.
-        define(factory);
-    } else if (typeof exports === 'object') {
-        // Node. Does not work with strict CommonJS, but
-        // only CommonJS-like enviroments that support module.exports,
-        // like Node.
-        module.exports = factory();
-    } else {
-        // Browser globals (root is window)
-        root.returnExports = factory();
-  }
-}(this, function () {
+(function () {
 
 var call = Function.prototype.call;
 var prototypeOfObject = Object.prototype;
@@ -553,4 +535,4 @@ if (!Object.isExtensible) {
     };
 }
 
-}));
+})(this);
